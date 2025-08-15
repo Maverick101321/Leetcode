c4edf97b71e8235b449406d82401adbae2138a13
Varun Choudhary <92911349+Maverick101321@users.noreply.github.com>
2025-08-15 11:28:07 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (86.68%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,13 +1,4 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        mid_index = len(nums) // 2
-        hashmap = {}
-        for i in nums:
-            if i not in hashmap:
-                hashmap[i] = 1
-            else:
-                hashmap[i] += 1
-        for key, value in hashmap.items():
-            if value > mid_index:
-                return key
-        return -1
+        nums.sort()
+        return nums[len(nums) // 2]
